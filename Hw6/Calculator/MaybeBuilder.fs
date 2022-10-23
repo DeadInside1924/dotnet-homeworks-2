1b0b10660fc00264b61058c0d11b0edf01fe688f
DeadInside1924 <nizamutdinov1924@gmail.com>
2022-10-23 21:29:06 +0300

    Hw6

---
+++ Hw6/Calculator/MaybeBuilder.fs
@@ -0,0 +1,13 @@
+module Hw6.MaybeBuilder
+
+open System
+
+type MaybeBuilder() =
+    member builder.Bind(a, f): Result<'e,'d> =
+        match a with
+        | Ok x -> f x
+        | Error err -> Error err
+        
+    member builder.Return x: Result<'a,'b> =
+        Ok x
+let maybe = MaybeBuilder()
