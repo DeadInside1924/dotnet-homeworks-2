6f06e120e88ab910085e45e55602858fb6e3e1ae
DeadInside1924 <nizamutdinov1924@gmail.com>
2022-11-22 15:46:44 +0300

    Changed extensions

--- Hw10/Configuration/ServiceCollectionExtensions.cs
+++ Hw10/Configuration/ServiceCollectionExtensions.cs
@@ -2,6 +2,10 @@ using Hw10.DbModels;
 using Hw10.Services;
 using Hw10.Services.CachedCalculator;
 using Hw10.Services.MathCalculator;
+using Hw10.Services.MathCalculator.ExpressionParser;
+using Hw10.Services.MathCalculator.ExpressionTreeBuilder;
+using Hw10.Services.MathCalculator.ExpressionValidator;
+using Hw10.Services.MathCalculator.GraphBuilder;
 
 namespace Hw10.Configuration;
 
@@ -9,7 +13,12 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddMathCalculator(this IServiceCollection services)
     {
-        return services.AddTransient<MathCalculatorService>();
+        return services
+            .AddTransient<MathCalculatorService>()
+            .AddTransient<IGraphBuilder, GraphBuilder>()
+            .AddSingleton<IExpressionValidator, ExpressionValidator>()
+            .AddSingleton<IExpressionTreeBuilder, ExpressionTreeBuilder>()
+            .AddSingleton<IExpressionParser, ExpressionParser>();
     }
     
     public static IServiceCollection AddCachedMathCalculator(this IServiceCollection services)
